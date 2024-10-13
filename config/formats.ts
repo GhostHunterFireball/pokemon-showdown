b61cb19e0222800df52809f98c5b48eee564c53e
GhostHunterFireball <157861256+GhostHunterFireball@users.noreply.github.com>
2024-10-13 14:43:45 +0200

    Update formats.ts
    Included battle factory format

--- config/formats.ts
+++ config/formats.ts
@@ -4259,4 +4259,10 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		battle: {trunc: Math.trunc},
 		ruleset: ['HP Percentage Mod', 'Cancel Mod', 'Desync Clause Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
+	{
+		name: "[Gen 3] Battle Factory",
+		mod: 'gen3',
+		searchShow: false,
+		ruleset: ['Obtainable'],
+	}
 ];
